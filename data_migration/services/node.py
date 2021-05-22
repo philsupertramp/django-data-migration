4b5d1fda9716df004a028f7579e97025a1b94014
Philipp Zettl <zettl.philipp@gmail.com>
2021-05-22 22:45:41 +0200

    replace classmethod

--- data_migration/services/node.py
+++ data_migration/services/node.py
@@ -1,7 +1,23 @@
 from django.apps.registry import Apps
 from django.db import models, connections, DatabaseError
 from django.utils import timezone
-from django.utils.functional import classproperty
+
+
+class classproperty:
+    """
+    Decorator that converts a method with a single cls argument into a property
+    that can be accessed directly from the class.
+    """
+    def __init__(self, method=None):
+        self.fget = method
+
+    def __get__(self, instance, cls=None):
+        return self.fget(cls)
+
+    def getter(self, method):
+        self.fget = method
+        return self
+
 
 
 class AlreadyAppliedError(Exception):
